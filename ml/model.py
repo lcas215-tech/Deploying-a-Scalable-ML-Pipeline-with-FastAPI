2a17fd5a1773ca3b3875dd31491c1dfa8593fa7a
Lcas215 <lcas215@wgu.edu>
2025-11-22 15:44:23 -0800

    created train_model function & inference function

--- ml/model.py
+++ ml/model.py
@@ -1,6 +1,6 @@
 import pickle
 from sklearn.metrics import fbeta_score, precision_score, recall_score
-from ml.data import process_data
+from data import process_data
 from time import time
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import StandardScaler
@@ -107,9 +107,8 @@ def save_model(model, path):
         Path to save pickle file.
     """
 
-
-
-
+    with open(path,'wb') as file:
+        pickle.dump(model, file)
 
 
     # TODO: implement the function
@@ -119,7 +118,11 @@ def save_model(model, path):
 def load_model(path):
     """ Loads pickle file from `path` and returns it."""
     # TODO: implement the function
-    pass
+
+    with open(path,'rb') as file:
+
+       model = pickle.load(file)
+    return model
 
 
 def performance_on_categorical_slice(
@@ -160,10 +163,13 @@ def performance_on_categorical_slice(
     """
     # TODO: implement the function
     X_slice, y_slice, _, _ = process_data(
-        # your code here
-        # for input data, use data in column given as "column_name", with the slice_value 
-        # use training = False
+        data[data[column_name] == slice_value],
+        categorical_features=categorical_features,
+        label=label,
+        training=False,
+        encoder=encoder,
+        lb=lb,
     )
-    preds = None # your code here to get prediction on X_slice using the inference function
+    preds = inference(model, X_slice) # your code here to get prediction on X_slice using the inference function
     precision, recall, fbeta = compute_model_metrics(y_slice, preds)
     return precision, recall, fbeta
